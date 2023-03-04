96dff869cb0ec04ec6f9437bb3b261e9c75c722f
MiguelAmoedo <miguelbombs@gmail.com>
2023-03-04 10:19:04 -0400

    first commit

---
+++ controllers/presentes.js
@@ -0,0 +1,47 @@
+module.exports = function (app) {
+    var PresentesController = {
+        index: function (req, res) {
+            res.render("presentes", {presentes: req.session.lista.presentes}); },
+        incluir: function (req, res) {
+            var lista = req.session.lista;
+            var presente = {
+                nome: req.body.nome,
+                valor: req.body.valor
+             };
+             lista.presentes.push(presente);
+             console.log("Presente carregado na lista");
+          
+            PresentesController.incluirnobanco(req,res);
+           
+              },
+        comprar: function (req, res) {
+            var id = req.params.id;
+            req.session.lista.presentes[id].comprado = true;
+            res.redirect("/presentes"); },
+        deletar: function (req,res) {
+            const id = +req.params.id;
+            const lista = req.session.lista;
+
+            delete lista.presentes[id];
+
+            res.redirect("/presentes");
+
+        },
+        listar: function (req, res) {
+                (async () => {
+                    const db = require("../db");
+                    const presentes = await db.execSQLQuery('SELECT * FROM presentes', res);                   
+                })();
+                },
+                incluirnobanco: function (req, res) {
+                    (async () => {
+                        const db = require("../db");
+                        console.log('conectou pela API');                
+                        const nome = req.body.nome;
+                        const valor = req.body.valor;
+                        const status = 1;
+                        const presentes = await db.execSQLQuery(`INSERT INTO presentes(Nome, Valor, Status) VALUES('${nome}','${valor}','${status}')`, res);
+                    })(); }
+    };
+    return PresentesController;
+};
